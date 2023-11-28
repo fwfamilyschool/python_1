fe56ba4fd1b5d5c0c64ac7d38914fd2bc5fe523d
fwfamilyschool@gmail.com <fwfamilyschool@gmail.com>
2023-11-28 10:44:03 -0600

    quick fix for Myron's game syntax

--- Myron/new_game.py
+++ Myron/new_game.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+
 def room_1():
     print("You wake up on a cold hard floor made of some kind of blue stone you dont know how you got here. You see that on the wall there is a message writen saying. 'This is the maze of Undour. solze all the puzzles then you can leave, fail snd you will be forgotten from all existence.' you look in shock at the message, you rub your eyes and look again but the message is gone, where the message was there is now a door.")
     choice = input("go through the door?:")
     if choice == "yes":
         print(" realising that there is nothing else you can do, you go through the door.")
         room_2()
+        
+        
 def room_2():
     print("as you go through the door it closses behind you with hardly a sound, infront of you there are three goblets, one of gold, one of silver and one made of a clear crystle. On the wall there is another message saying. '' ")
+    
+if __name__ == "__main__":
+    room_1()
