81f1fed4b9e0df5e2f0fe263b981688400a86165
fwfamilyschool <fwfamilyschool@gmail.com>
2023-12-05 10:34:37 -0600

    uppdated scene_1

--- Tristan/scene_1.py
+++ Tristan/scene_1.py
@@ -26,9 +26,8 @@ def ironville():
         print('in front of you you see a crossroads. You can go east west or south')
     else:
         print('You exit the town and head to the mine.')    
+iron_mine_entrance()
 
-     
-    
 def main():
     ironville()
 #     my_list = "alpha beta gamma delta foo bar baz".split() 
@@ -52,20 +51,25 @@ leading into the mine.""")
     action = input("Choose enter or leave: ")
     if action == "enter":
         iron_mine_room()
+    else:
+        print("You go back to Ironville...coward")
+        ironville()
 def iron_mine_room():
         print("""Entering the mine you see you see a pickaxe on the ground.
 The air is damp. The rails are blocked by a barricade.""")
         action = input ("Do You want take the pickaxe?")
         if action == "yes":
             print("You use the pickaxe to break through the barricade.")
-        else: 
-            action = input ("You take apart the barricade of stone ")
-    else:
-        print("You go back to Ironville...coward")
-        leave = ironville()
-         main_room():
+        elif: 
+            print("You take apart the barricade of stone ")
+        else:
+            print("You go back to Ironville...coward")
+            leave = ironville()
+       main_room():
 
 def main_room():
+    hitpoints=100
+    resistance=15
     print("""You see a Goblin in the middle of the room. He has his back
 to you. Since he heard you break throught the barricade he quickly turns
 around. """)
@@ -73,9 +77,7 @@ around. """)
     if action == "attack":
         print("You attack the goblin")
         print("You defeat the goblin")
-    else:
-        print(" Talk to goblin")
-        print("The goblin attacks")
+        print("you lose your shield")
     oval_room()
 def oval_room():
     print("""Enter an oval room. There are 2 Goblins in the room:
@@ -99,32 +101,38 @@ Choose: You proceed to tunnel to the South""")
  room_4()   
 def room_4():
     print("""Heading south, deeper in the mine, you see 2 orcs.
-Options: attack, Talk, Sneak or Leave
-Choose: attack and you Succeed""")
-    
+    Options: attack, Talk, Sneak or Leave
+    Choose: attack and you Succeed""")
+   room_5()
 def room_5():
     print("""Continuing down the tunnel. Continue forward or Turn East.
-Choose: turn East""") ("""You see wood holding the roof and you see torchis
-on the wall and you see the ground is rocky""")
-
+    Choose: turn East""") ("""You see wood holding the roof and you see torchis
+    on the wall and you see the ground is rocky""")
+room_6()
 def room_6():
     print("""Leads you to another room that has 2 Orcs and 1 Gobblin. 
 Options: attack, Talk, or Leave
 Choose: attack and you Succeed""") ("""After you attack them you see iorn in the walls and on
     the floor thare is rooks and ther is wood holding the roof and you see and you see the ground is rocky""")
     ("""Return to the main hallway.
- Head North back to entrance, Head East back to room, Head south going further in mine.
-Head south going further in mine
-Entering another room you meet another human. They are able to heal you. and you see torchis in this room
-and you see
-wood holding the roof and you see and you see the ground is rocke""")
+    Head North back to entrance, Head East back to room, Head south going further in mine.
+    Head south going further in mine
+    Entering another room you meet another human. They are able to heal you. and you see    torchis in this room
+    and you see
+    wood holding the roof and you see and you see the ground is rocke""")
+
+room_7():
 def room_7():
     print("""Return to the main hallway.
 Options: Head North back to entrance, Head East back to room, Head south
 going further in mine.""")
+
+room_8()
 def room_8():
     print("""Choose: Head south going further in mine
 Entering another room """)
+
+room_9()
 def room_9():
     print("""Enter tunnels on East, West or South.Choose: You proceed to
 tunnel to the West. or you go south then you see 3 goblin  you see a big room with
@@ -138,7 +146,7 @@ the roof and you see and you see the ground is smoth""")
 if __name__ == "__main__":
     main()
 
-
+hitpoi
 
 
 
